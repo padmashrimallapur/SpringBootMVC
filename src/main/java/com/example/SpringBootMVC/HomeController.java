0028bb8c2b937fba4511e2032349fa0aa794a061
Padmashri Mallapur <padmashri.allapur@gmail.com>
2024-02-24 23:00:33 -0500

    - Created a simple form to enter 2 numbers - On click of submit button, adding of two number result will be displayed
    This is to demonstrate how to use HttpServeltRequest, HttpSession

--- src/main/java/com/example/SpringBootMVC/HomeController.java
+++ src/main/java/com/example/SpringBootMVC/HomeController.java
@@ -1,6 +1,8 @@
 package com.example.SpringBootMVC;
 
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping("/")
-    public void home(){
-        System.out.println("home Page is requested");
+    public String home(){
+        return "index.jsp";
+    }
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest req){
+
+        int i = Integer.parseInt(req.getParameter("num1"));
+        int j = Integer.parseInt(req.getParameter("num2"));
+
+        int res = i+j;
+        HttpSession session  = req.getSession();
+        session.setAttribute("res", res);
+
+        return "results.jsp";
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,11 +1,16 @@
-<%@ page language='java' contentType='text/html charset=UTF-8' pageEncoding = "UTF-8"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
 <!DOCTYPE html>
 <html>
 <head>
-<meta charset="UTF-8">
-<title>Insert Title</title>
+    <meta charset="UTF-8">
+    <title>Insert Title</title>
 </head>
 <body>
    Welcome to Spring MVC
+      <form action= "add">
+       Enter first number : <input type="text" name="num1"><br>
+       Enter second number : <input type="text" name="num2"><br>
+       <input type="submit">
+       </form>
 </body>
 </html>
